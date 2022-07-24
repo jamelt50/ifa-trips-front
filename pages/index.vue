2f067e30c7b6ecd83be7889a0df53cd2b67150bc
jamalt50 <jamal.bouhaouliane@gmail.com>
2022-07-24 21:54:03 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="w-full min-h-screen bg-green-600">
+<div class="w-32 h-32 bg-white relative">
+<nav class="absolute h-10 bg-gray-400"> <ul class="flex"><li></li></ul></nav>
+
+</div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
